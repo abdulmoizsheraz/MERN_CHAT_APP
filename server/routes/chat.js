3586bdb0aaee74562df116afd67d0cf705c5ed76
abdulmoizsheraz <moiz77131@gmail.com>
2024-06-04 13:12:36 +0500

    feat:ChatRoutes

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { newGroupChat,getMyChats,getMyGroups } from "../controllers/chat.js";
+import { newGroupChat,getMyChats,getMyGroups,addMembers,removeMember,leaveGroup } from "../controllers/chat.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
@@ -7,9 +7,15 @@ const router = express.Router();
 router.use(isAuthenticated);
 
 router.post('/newChat', newGroupChat);
-router.get("/mygrps", getMyChats);
-app.get("/my/groups", getMyGroups);
+router.get("/my", getMyChats);
+router.get("/my/groups", getMyGroups); // grps that are created my me
+router.put("/addmembers", addMembers);
 
+router.put(
+  "/removemember",
+  removeMember
+);
+router.delete("/leave/:id", leaveGroup);
 
 
 export default router;
